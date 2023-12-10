4db34a1ef5bf0e925e50270edd34e61296be5c66
yousefmohamed3040 <ym4073157@gmail.com>
2023-12-10 23:44:10 +0200

    task1

--- task1/2/main.c
+++ task1/2/main.c
@@ -3,12 +3,15 @@
 
 int main()
 {
-    int n1,n2;
+    int n1,n2,x;
     printf("enter number1:");
     scanf("%d",&n1);
     printf("enter number2:");
     scanf("%d",&n2);
     printf("the numbers before swaping are %d %d\n",n1,n2);
-    printf("the numbers after swaping are %d %d",n2,n1);
+    x=n1;
+    n1=n2;
+    n2=x;
+    printf("the numbers after swaping are %d %d",n1,n2);
     return 0;
 }
